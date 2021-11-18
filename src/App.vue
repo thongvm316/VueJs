1700cd30419ece2eeab61a5178acaeaca78b9e0e
thongvm316 <vominhthong1603@gmail.com>
2021-11-18 16:21:08 +0700

    method and Event Handling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
-  
+  <h2>Add method - {{ add(1, 5, 9) }}</h2>
+  <h2>Multiplier method - {{ multiply(9) }}</h2>
+  <h2>Multiplier method - {{ multiply(baseValue) }}</h2>
+
+  <h2>{{ name }}</h2>
+  <div>
+    <button v-on:click="changeName($event), increment(1, $event)">Click</button>
+    <button v-on:mouseover="changeName">Mouse Over</button>
+  </div>
+
+  <h2>{{ count }}</h2>
+  <div>
+    <button v-on:click="increment(1, $event)">Increment</button>
+    <button v-on:click="decrement(5)">Decrement</button>
+
+    <!-- Can usee @click instead v-on:click -->
+    <!-- <button @click="increment(1)">Increment</button>
+    <button @click="decrement(5)">Decrement</button> -->
+  </div>
 </template>
 
 <script>
@@ -8,7 +26,29 @@ export default {
   name: 'App',
   data() {
     return {
-      
+      name: 'Thong',
+      count: 0,
+      baseMultiplier: 5,
+      baseValue: 10
+    }
+  },
+  methods: {
+    add(a, b, c) {
+      return a + b + c
+    },
+    multiply(num) {
+      return num * this.baseMultiplier
+    },
+    increment(num, event) {
+       this.count += num
+       console.log(event)
+    },
+    decrement(num) {
+       this.count -= num
+    },
+    changeName(event) {
+      this.name = 'Batman'
+      console.log(event)
     }
   }
 }
@@ -22,5 +62,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+  }
 </style>
