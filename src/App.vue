ab63f791802cc5c4889fd9268f9da861c53cc30f
thongvm316 <vominhthong1603@gmail.com>
2021-11-20 05:32:12 +0700

    compoent-vmodal

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,21 @@
 <template>
   <div>
-    <h3>App Comp username {{ name }}</h3>
-    <ComponentC/>
-
-    <button @click="showPopup = true">Show Popup</button>
-    <Popup v-show="showPopup" @close="closePopup"/>
-
-    
+    <Input v-model="name"/>
   </div> 
 </template>
 
 <script>
-  import ComponentC from './components/ComponentC.vue'
-  import Popup from './components/Popup.vue'
-
+  import Input from './components/Input.vue'
   export default {
     name: 'App',
     components: {
-      ComponentC,
-      Popup
+     Input
     },
     data() {
       return {
-        name: 'minh', 
-        channel: 'R316',
-        showPopup: false
+        name: 'thong', 
       }
     },
-    methods: {
-      closePopup(name) {
-        this.showPopup= false
-        console.log(name) // show thongvm, value of params in $emit fnc in Popup.vue file
-      },
-    },
-    provide() {
-      return {
-        username: this.name
-      }
-    }
   }
 </script>
 
@@ -49,32 +27,5 @@
   /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
-  }
-
-  label {
-    font-weight: bold;
-    display: flex;
-    margin-bottom: 5px;
-  }
-
-  input + label {
-    font-weight: bold;
-    display:inline-flex;
-    margin-right: 20px;
-  }
-
-  input[type='text'],
-  textarea,
-  select {
-    display: block;
-    width: 400px;
-    padding: 6px 12px;
-    font-size: 14px;
-    line-height: 1.42;
-    color: #555;
-    background-color: #fff;
-    background-image: none;
-    border: 1px solid #ccc;
-    border-radius: 4px;
   }
 </style>
