682c306f969e38c9fc217d1f0508fdf3e3c04b43
thongvm316 <vominhthong1603@gmail.com>
2021-11-19 11:30:39 +0700

    fn ls 25

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,21 @@
       <h2> Computed FullName - {{ fullName }}</h2>
 
       <button @click="items.push({id: 4, title: 'Keyboard', price: 50})">Add item</button>
-      <h2>Total - {{ total }}</h2>
+      <h2>Computed Total - {{ total }}</h2>
+      <h2>Methods Total - {{ getTotal() }}</h2>
+      <!-- What is different betweent Computed and Methods? Res: Computed catch result for next render to improve performce, Methods is not., type input and check clg for test behavior -->
+
+      <input type="text" v-model="country" name="" id="">
+
+      <template v-for="item in items" :key="item.id">
+        <h2 v-if="item.price > 200">{{item.title}}  {{item.price}}</h2>
+      </template>
+
+      <h2 v-for="item in expensiveItems" :key="item.id">{{item.title}}  {{item.price}}</h2>
     </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data() {
@@ -32,18 +41,26 @@ export default {
          title: 'SDTV',
          price: 400
        },
-     ]
+     ],
+     country: ''
     }
   },
   methods: { 
-   
+    getTotal() {
+      console.log('getTotal')
+      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
+    }
   },
   computed: {
     fullName () {
       return `${this.firstName} ${this.lastName}`
     },
     total () {
+      console.log('computed')
       return this.items.reduce((total, curr) => (total = total + curr.price), 0)
+    },
+    expensiveItems () {
+      return this.items.filter(item => item.price > 200)
     }
   }
 }
