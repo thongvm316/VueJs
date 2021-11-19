ec0b6f20e4107820b700095649702aa72031c519
thongvm316 <vominhthong1603@gmail.com>
2021-11-20 04:33:31 +0700

    provide/inject

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
 <template>
   <div>
-    <!-- <Greet name="thong" heroName='minh'/>
-    <Greet name="hana" heroName='quynh'/>
-    <Greet name="phat" heroName='vo'/> -->
+    <h3>App Comp username {{ name }}</h3>
+    <ComponentC/>
 
-    <Article id="my-article" title="Article title" :likes="50" :isPublic="true"/> 
-  </div> <!-- Just use when component has a wraper div -->
+    <button @click="showPopup = true">Show Popup</button>
+    <Popup v-show="showPopup" @close="closePopup"/>
+
+    
+  </div> 
 </template>
 
 <script>
-// import Greet from './components/Greet.vue'
-import Article from './components/Article.vue'
+  import ComponentC from './components/ComponentC.vue'
+  import Popup from './components/Popup.vue'
 
-export default {
-  name: 'App',
-  components: {
-    // Greet,
-    Article
-  },
-  data() {
-    return {
-      name: 'minh',
-      channel: 'R316'
+  export default {
+    name: 'App',
+    components: {
+      ComponentC,
+      Popup
+    },
+    data() {
+      return {
+        name: 'minh', 
+        channel: 'R316',
+        showPopup: false
+      }
+    },
+    methods: {
+      closePopup(name) {
+        this.showPopup= false
+        console.log(name) // show thongvm, value of params in $emit fnc in Popup.vue file
+      },
+    },
+    provide() {
+      return {
+        username: this.name
+      }
     }
   }
-}
 </script>
 
 <style>
