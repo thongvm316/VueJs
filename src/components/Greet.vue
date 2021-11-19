10056d4f2fd436a1a47755e44be7c8fd295415b7
thongvm316 <vominhthong1603@gmail.com>
2021-11-20 03:32:36 +0700

    components

---
+++ src/components/Greet.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2>Hello {{ name }} {{ heroName }}</h2>
+</template>
+
+<script>
+    export default {
+        name: 'Greet',
+        props: ['name', 'heroName']
+    }
+</script>
+
+<style scoped>
+
+</style>
