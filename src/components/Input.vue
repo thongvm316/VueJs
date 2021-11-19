47c2e01a4ef357cb4c9033f764e17797b8ea5ffe
thongvm316 <vominhthong1603@gmail.com>
2021-11-20 05:57:16 +0700

    fb

---
+++ src/components/Input.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Input',
+        props: {
+            modelValue: String
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
