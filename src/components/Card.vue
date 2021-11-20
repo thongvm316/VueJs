886cc28dec35d1b0989f36e613651e685476b213
thongvm316 <vominhthong1603@gmail.com>
2021-11-20 07:02:31 +0700

    slot

---
+++ src/components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div class="card">
+            <!-- <slot>Default content</slot> -->
+            <div id="card-header">
+                <slot name="header"></slot>
+            </div>
+            <div id="card-content">
+                <slot></slot>
+            </div>
+            <div id="card-footer">
+                <slot name="footer"></slot>
+            </div>
+
+            <slot name="test"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Card',
+        props: ['contents']
+    }
+</script>
+
+<style scoped>
+.card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    transition: .3s;
+    padding: 16px;
+    margin-bottom: 20px;
+    width: 200px;
+}
+
+.card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+
+</style>
