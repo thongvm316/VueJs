83062440af9dd0632214f239cd9c20a97e91a668
thongvm316 <vominhthong1603@gmail.com>
2021-11-22 09:46:47 +0700

    td

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
-  <div>
-   <PostList/>
-   <CreatePost/>
+  <div> 
+    <Parent/>
   </div> 
 </template>
 
 <script>
-  import PostList from './components/PostList.vue'
-  import CreatePost from './components/CreatePost.vue'
+  import Parent from './components/Parent.vue';
 
   export default {
     name: 'App',
     components: {
-    PostList,
-    CreatePost
+      Parent
     },
     data() {
       return { 
@@ -21,7 +18,7 @@
       }
     },
   }
-</script>
+</script> 
 
 <style>
 #app {
---
+++ src/components/Parent.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        Parent component
+        <button @click="showChild = !showChild">Button</button>
+        <Child v-if="showChild"/>
+    </div>
+</template>
+
+<script>
+    import Child from './Child.vue'
+
+    export default {
+        name: 'Parent',
+        components: {
+            Child
+        },
+        data() {
+            return {
+                showChild: true
+            }
+        },
+        beforeCreate() {
+            console.log('Parent beforeCreate')
+        },
+        created() {
+            console.log('Parent created')
+        },
+        beforeMount() {
+            console.log('Parent beforeMount')
+        },
+        mounted() {
+            console.log('Parent mounted')
+        },
+        beforeUpdate() {
+            console.log('Parent beforeUpdate')
+        },
+        updated() {
+            console.log('Parent updated')
+        },
+        beforeUnmount(){
+            console.log('Parent beforeUnmount')
+        },
+        unmounted() {
+            console.log('Parent unmounted')
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
