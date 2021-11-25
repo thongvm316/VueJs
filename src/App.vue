6a9c703755ffa0ad269b107e382010def60f7640
thongvm316 <vominhthong1603@gmail.com>
2021-11-25 17:48:30 +0700

    fix st

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <!-- <Greet name="thong" heroName='minh'/>
-    <Greet name="hana" heroName='quynh'/>
-    <Greet name="phat" heroName='vo'/> -->
+    <Greet name="thong" heroName="minh" />
+    <Greet name="hana" heroName="quynh" />
+    <Greet name="phat" heroName="vo" />
 
-    <Article id="my-article" title="Article title" :likes="50" :isPublic="true"/> 
-  </div> <!-- Just use when component has a wraper div -->
+    <Article
+      id="my-article"
+      title="Article title"
+      :likes="50"
+      :isPublic="true"
+    />
+    <!-- :likes, :isPublic use for number and boolean -->
+  </div>
+  <!-- Just use when component has a wraper div -->
 </template>
 
 <script>
@@ -16,14 +23,14 @@ export default {
   name: 'App',
   components: {
     // Greet,
-    Article
+    Article,
   },
   data() {
     return {
       name: 'minh',
-      channel: 'R316'
+      channel: 'R316',
     }
-  }
+  },
 }
 </script>
 
@@ -35,32 +42,32 @@ export default {
   /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
-  }
+}
 
-  label {
-    font-weight: bold;
-    display: flex;
-    margin-bottom: 5px;
-  }
+label {
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
+}
 
-  input + label {
-    font-weight: bold;
-    display:inline-flex;
-    margin-right: 20px;
-  }
+input + label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
+}
 
-  input[type='text'],
-  textarea,
-  select {
-    display: block;
-    width: 400px;
-    padding: 6px 12px;
-    font-size: 14px;
-    line-height: 1.42;
-    color: #555;
-    background-color: #fff;
-    background-image: none;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-  }
+input[type='text'],
+textarea,
+select {
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
 </style>
